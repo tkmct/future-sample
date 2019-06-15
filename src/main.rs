f1b244c3a703c976b1e56bd5604e3c1ed183d009
Takamichi Tsutsumi <1220t.takamichi@gmail.com>
2019-06-16 03:02:01 +0900

    use callback

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,12 @@ extern crate web3;
 
 use futures::{future, Async, Future, Poll, Stream};
 use std::fmt;
+use std::marker::Send;
 use std::time::Duration;
 use tokio::timer::Interval;
 
-use web3::types::FilterBuilder;
+use web3::types::{FilterBuilder, Log};
 use web3::{transports, Web3};
-
-struct Event {
-    pub name: String,
-}
-
-impl Event {
-    pub fn new(name: String) -> Self {
-        Event { name }
-    }
-}
 
 impl fmt::Display for Event {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -27,83 +18,101 @@ impl fmt::Display for Event {
     }
 }
 
-
-struct EventWatcher {
+struct EventFetcher {
     interval: Interval,
     web3: Web3<transports::Http>,
 }
 
-impl EventWatcher {
-    pub fn new(duration: Duration, web3: Web3<transports::Http>) -> Self {
-        EventWatcher {
+impl EventFetcher {
+    pub fn new(web3: Web3<transports::Http>, duration: Duration) -> Self {
+        EventFetcher {
             interval: Interval::new_interval(duration),
             web3,
         }
     }
 }
 
-impl Stream for EventWatcher {
-    type Item = Event;
+impl Stream for EventFetcher {
+    type Item = Vec<Log>;
     type Error = ();
 
-
-    fn poll(&mut self) -> Poll<Option<Event>, ()> {
-        println!("poll");
+    fn poll(&mut self) -> Poll<Option<Vec<Log>>, ()> {
         let filter = FilterBuilder::default().build();
         try_ready!(self.interval.poll().map_err(|_| ()));
-        let logs = self.web3.eth().logs(filter).wait().map_err(|_| ());
-        println!("{:?}", logs);
 
-        let event = Event::new("ValueSet".to_owned());
-        Ok(Async::Ready(Some(event)))
+        let logs = match self.web3.eth().logs(filter).wait().map_err(|_| ()) {
+            Ok(v) => Some(v),
+            Err(_) => None,
+        };
+
+        Ok(Async::Ready(logs))
     }
 }
 
-struct EventDisplay<T> {
-    stream: T,
-    curr: usize,
+struct EventWatcher {
+    stream: EventFetcher,
+    // TODO: make listeners future
+    listeners: Vec<Box<dyn Fn(&Log) -> () + Send>>,
 }
 
-impl<T> EventDisplay<T> {
-    fn new(stream: T) -> EventDisplay<T> {
-        EventDisplay { stream, curr: 0 }
+impl EventWatcher {
+    fn new(stream: EventFetcher) -> EventWatcher {
+        EventWatcher {
+            stream,
+            listeners: vec![],
+        }
+    }
+
+    // TODO: make listeners future
+    fn subscribe(&mut self, listener: Box<dyn Fn(&Log) -> () + Send>) {
+        self.listeners.push(listener);
     }
 }
 
-impl<T> Future for EventDisplay<T>
-where
-    T: Stream,
-    T::Item: fmt::Display,
-{
+impl Future for EventWatcher {
     type Item = ();
-    type Error = T::Error;
+    type Error = ();
 
     fn poll(&mut self) -> Poll<(), Self::Error> {
-        while self.curr < 100 {
-            let value = match try_ready!(self.stream.poll()) {
+        loop {
+            let logs = match try_ready!(self.stream.poll()) {
                 Some(value) => value,
                 None => break,
             };
 
-            println!("value #{} = {}", self.curr, value);
-            self.curr += 1;
+            for log in logs.iter() {
+                // TODO: make listeners future
+                for listener in self.listeners.iter() {
+                    listener(log);
+                }
+            }
         }
 
         Ok(Async::Ready(()))
     }
 }
 
-
 fn main() {
     println!("Started");
     let (_eloop, transport) = web3::transports::Http::new("http://localhost:9545").unwrap();
     let web3 = web3::Web3::new(transport);
-    let watcher = EventWatcher::new(Duration::from_secs(1), web3);
-    let displayer = EventDisplay::new(watcher);
+    let fetcher = EventFetcher::new(web3, Duration::from_secs(1));
+    let mut watcher = EventWatcher::new(fetcher);
 
+    watcher.subscribe(Box::new(|_log| {
+        println!("Listener fire");
+    }));
+
+    watcher.subscribe(Box::new(|_log| {
+        println!("Listener2 fire");
+    }));
+
+    watcher.subscribe(Box::new(|_log| {
+        println!("Listener3 fire");
+    }));
 
     tokio::run(future::lazy(|| {
-        tokio::spawn(displayer);
+        tokio::spawn(watcher);
         Ok(())
     }));
     println!("Terminated");
